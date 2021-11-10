856f95d90b826f8ac7de64b8412e055dc7a193a1
Carlos Haupter Pérez <haupterperezcarlos@gmail.com>
2021-11-10 14:40:37 +0100

    Creada lista de búsqueda

--- lib/search/search_delegate.dart
+++ lib/search/search_delegate.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:peliculas/models/models.dart';
 import 'package:peliculas/providers/movies_provider.dart';
+import 'package:provider/provider.dart';
 
 class MovieSearchDelegate extends SearchDelegate {
   @override
@@ -31,31 +34,55 @@ class MovieSearchDelegate extends SearchDelegate {
     return Text("BuildResults");
   }
 
+  Widget _emptyContainer() {
+    return Container(
+      child: Center(
+        child: Icon(
+          Icons.movie_creation_outlined,
+          color: Colors.black38,
+          size: 130,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget buildSuggestions(BuildContext context) {
+    final _moviesProvider = Provider.of<MoviesProvider>(context, listen: false);
 
-    final _moviesProvider= MoviesProvider();
+    return FutureBuilder(
+        future: _moviesProvider.searchMovies(query),
+        builder: (_, AsyncSnapshot<List<Movie>> snapshot) {
+          if (!snapshot.hasData) {
+            return _emptyContainer();
+          }
 
-    if (query.isEmpty) {
-      return Container(
-        child: Center(
-          child: Icon(
-            Icons.movie_creation_outlined,
-            color: Colors.black38,
-            size: 130,
-          ),
-        ),
-      );
-    }
+          final List<Movie> movie = snapshot.data!;
 
-    return Container(
-      child: Column(
-        children: [
-          FutureBuilder(
-            future: _moviesProvider.searchMovies(query),
-            builder: )
-        ],
+          return ListView.builder(
+              itemCount: movie.length,
+              itemBuilder: (_, int index) => _MovieItem(movie[index]));
+        });
+  }
+}
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: FadeInImage(
+        fit: BoxFit.contain,
+        placeholder: AssetImage("assets/loading.gif"),
+        image: NetworkImage(movie.fullPosterImage),
+        width: 50,
       ),
+      title: Text(movie.title),
+      subtitle: Text(movie.originalTitle),
+      onTap: () => print(movie.title),
     );
   }
 }
